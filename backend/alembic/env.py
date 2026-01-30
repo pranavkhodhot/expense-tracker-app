9b83a866233b04f04b8c4cd8cbd6e01f72485c34
PranavKhodhot <pranavkhodhot@gmail.com>
2026-01-29 22:40:41 -0500

    Added CI Changes

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,7 +1,7 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from app.database import Base  
+from app.database import Base, DATABASE_URL
 from app.models import user, transaction, budget, category
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -17,6 +17,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
